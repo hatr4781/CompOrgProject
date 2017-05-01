4a055e92e879cb14287e8a22c1236bfc148995be
Ha Tran <Ha Tran>
2017-05-01 11:33:56 -0600

    Finished Mem Stage, added typedef for signed and unsiged in main.h

--- MEM.c
+++ MEM.c
@@ -1,21 +1,75 @@
 #include "MEM.h"
 
 //Accessing memory for load/store instructions
-struct Instruction* Instruction_Mem(struct Instruction* inst){
+struct Instruction* Instruction_Memory(struct Instruction* inst){
     uint32_t val;
+    int byte_offset;
+    int mem_word;
+    uint32_t mem_address;
+
+    mem_address = inst->mem_address;
+    mem_word = mem_address / 4;
+
+    int offset;
     if(inst->name == "LB"){
-        val = 0x000000ff & memory[inst->mem_address];
-        if((val & 0x80)== 1){
-            val = 0xffffff00 | val;
+        offset = mem_address % 4;
+
+        if(offset == 0){
+            val = BYTE_0 & memory[mem_word];
+            if(val & 0x00000080){
+                val = 0xffffff00 | val;
+            }
         }
-        else{
-            val = 0x00000000 & val;
+        else if(offset == 1){
+            val = (BYTE_1 & memory[mem_word]) >> 8;
+            if(val & 0x00000080){
+                val = 0xffffff00 | val;
+            }
         }
+        else if(offset == 2){
+            val = (BYTE_2 & memory[mem_word]) >> 16;
+            if(val & 0x00000080){
+                val = 0xffffff00 | val;
+            }
+        }
+        else if(offset == 3){
+            val = (BYTE_3 & memory[mem_word]) >> 24;
+            if(val & 0x00000080){
+                val = 0xffffff00 | val;
+            }
+        }
+        inst->rt_val = (SIGNED_WORD) val;
     }
+
     else if(inst->name == "LBU") {
-        val = 0x000000ff & memory[inst->mem_address];
+        offset = mem_address % 4;
+        if(offset == 0){
+            val = BYTE_0 & memory[mem_word];
+        }
+        else if(offset == 1){
+            val = (BYTE_1 & memory[mem_word]) >> 8;
+        }
+        else if(offset == 2){
+            val = (BYTE_2 & memory[mem_word]) >> 16;
+        }
+        else if(offset == 3){
+            val = (BYTE_3 & memory[mem_word]) >> 24;
+        }
+        inst->rt_val = (UNSIGNED_WORD) val;
     }
     else if(inst->name == "LHU"){
-        val = 0x0000ffff & memory[inst->mem_address];
+        offset = mem_word % 2;
+        if(offset == 0){
+            val = HALF_0 & memory[mem_word];
+        }
+        else if(offset == 1){
+            val = (HALF_1 & memory[mem_word]) >> 16;
+        }
+        inst->rt_val = (UNSIGNED_WORD) val;
     }
+    else if(inst->name == "LW"){
+        val = memory[mem_word];
+        inst->rt_val = (SIGNED_WORD) val;
+    }
+
 }
--- MEM.h
+++ MEM.h
@@ -8,6 +8,14 @@
 
 #include "main.h"
 
+#define BYTE_0 0x000000ff
+#define BYTE_1 0x0000ff00
+#define BYTE_2 0x00ff0000
+#define BYTE_3 0xff000000
+
+#define HALF_0 0x0000ffff
+#define HALF_1 0xffff0000
+
 struct Instruction* Instruction_Mem(struct Instruction* inst);
 
 #endif //COMPORGPROJECT_MEM_H
